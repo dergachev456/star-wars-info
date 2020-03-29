bb9f47c0fe0b3101134a2d52c81e705bc1a8312b
AlexanderDergachev <31667079+AlexanderDergachev@users.noreply.github.com>
2020-03-29 20:49:15 +0300

    Updates

---
+++ precache-manifest.96e161421ccb645099cd5e57142a6680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c40081127df00d0d00072db0b33124",
+    "url": "/star-wars-info/index.html"
+  },
+  {
+    "revision": "b26c018e2ad426edc426",
+    "url": "/star-wars-info/static/css/main.7162bd2c.chunk.css"
+  },
+  {
+    "revision": "599b3e9940cc1efe3127",
+    "url": "/star-wars-info/static/js/2.08be802e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/star-wars-info/static/js/2.08be802e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b26c018e2ad426edc426",
+    "url": "/star-wars-info/static/js/main.01a29a30.chunk.js"
+  },
+  {
+    "revision": "aff9d19680fdcccde76a",
+    "url": "/star-wars-info/static/js/runtime-main.a896ff10.js"
+  }
+]);
